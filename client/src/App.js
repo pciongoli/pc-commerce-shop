164bc9c403769ceb8c38fbeaf7531317ca2c5851
pcion <patrick.ciongoli@gmail.com>
2023-03-01 15:43:31 -0800

    deploying

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,8 @@ import NoMatch from './pages/NoMatch';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Nav from './components/Nav';
+import { StoreProvider } from './utils/GlobalState';
+import Success from './pages/Success';
 import OrderHistory from './pages/OrderHistory';
 
 const httpLink = createHttpLink({
@@ -40,33 +42,39 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
         <div>
-          <Nav />
-          <Routes>
-            <Route 
-              path="/" 
-              element={<Home />} 
-            />
-            <Route 
-              path="/login" 
-              element={<Login />} 
-            />
-            <Route 
-              path="/signup" 
-              element={<Signup />} 
-            />
-            <Route 
-              path="/orderHistory" 
-              element={<OrderHistory />} 
-            />
-            <Route 
-              path="/products/:id" 
-              element={<Detail />} 
-            />
-            <Route 
-              path="*" 
-              element={<NoMatch />} 
-            />
-          </Routes>
+          <StoreProvider>
+            <Nav />
+            <Routes>
+              <Route 
+                path="/" 
+                element={<Home />} 
+              />
+              <Route 
+                path="/login" 
+                element={<Login />} 
+              />
+              <Route 
+                path="/signup" 
+                element={<Signup />} 
+              />
+              <Route 
+                path="/success" 
+                element={<Success />} 
+              />
+              <Route 
+                path="/orderHistory" 
+                element={<OrderHistory />} 
+              />
+              <Route 
+                path="/products/:id" 
+                element={<Detail />} 
+              />
+              <Route 
+                path="*" 
+                element={<NoMatch />} 
+              />
+            </Routes>
+          </StoreProvider>
         </div>
       </Router>
     </ApolloProvider>
