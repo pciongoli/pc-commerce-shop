573f0f6673df7179a85c1ae3afb9323b973ceb08
pcion <patrick.ciongoli@gmail.com>
2023-03-01 15:51:49 -0800

    attempting to deploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-   process.env.MONGODB_URI || "mongodb://localhost:27017/pc-commerce-shop",
+   process.env.MONGODB_URI || "mongodb://localhost:27017/cluster0",
    {
       useNewUrlParser: true,
       useUnifiedTopology: true,
